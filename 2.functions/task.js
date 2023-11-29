716227a6e577afcc2b24498c708fb9675a910201
Alex hitsaaja <sto98@inbox.ru>
2023-11-29 13:29:40 +0200

    Readmi 4 task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,9 +4,6 @@ function getArrayParams(...arr) {
 	let max = -Infinity;
 	let sum = 0;
 	for (let i = 0; i < arr.length; i++) {
-		if (arr <= 0) {
-			return 0;
-		}
 		sum += arr[i];
 	}
 	let avg = sum / arr.length;

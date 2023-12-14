5ae70f657e91b11dd0666382db8c8a6fa3619a86
Alex hitsaaja <sto98@inbox.ru>
2023-12-14 19:18:58 +0200

    Cheange readme task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,31 +1,29 @@
 function Student(name, gender, age) {
-    this.name=name;
-    this.gender=gender;
-    this.age=age;
-    this.marks=[];
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-    for (let grade of marks)
-    if ( this.marks === null ?? undefined) {
-        // this.marks = 1;
-        return 0; 
-        } else {
-        this.marks.push(grade); 
-        }
+Student.prototype.addMarks = function(...marks) {
+	for (const grade of marks)
+		Student.hasOwnProperty((this.marks) ? this.marks.push(grade) : 0);
 }
 
-Student.prototype.getAverage = function (marks) {
-    
-      return this.marks.reduce((acc,element,index,arr)=>acc+element/arr.length,0?acc:0)
-    }
-  
-Student.prototype.exclude = function (reason) {
-    delete this.marks;
-    delete this.subject;
-    this.excluded = reason; 
+Student.prototype.getAverage = function(marks) {
+	if (this.marks === undefined) {
+		return 0;
+	} else {
+		return this.marks.reduce((acc, element, index, arr) => acc + element / arr.length, 0 ? acc : 0);
+	}
+}
+
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }

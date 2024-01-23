4ed90de50ceedeb6cc4bd5fc1335c45b5ed8bebb
Alex hitsaaja <sto98@inbox.ru>
2024-01-24 00:20:16 +0200

    commit24010020 task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,93 +1,73 @@
 class AlarmClock {
-	constructor(intervalId = null) {
+	constructor() {
 		this.alarmCollection = [];
-		this.intervalId = intervalId;
+		this.intervalId = null;
 	}
 
 	/*`addClock` — добавляет новый звонок в коллекцию существующих. */
-	addClock(time, callback, canCall) {
-		this.time = time;
-		this.callback = callback;
-		this.canCall = true;
-
-		if (this.time === null || this.callback === undefined) {
+	addClock(time, callback) {
+		if (time === null || callback === undefined) {
 			throw new Error(" Отсутствуют обязательные аргументы ");
 		}
 
-		if (this.alarmCollection.filter(call => call === this.time)) {
+		if (this.alarmCollection.some(eliment => eliment.time === time)) {
 			console.warn(" Уже присутствует звонок на это же время ");
-		} else {
-			this.alarmCollection.push(time);
 		}
-
-
-		if (this.alarmCollection.forEach(call => call.time.getHours() === new Date().getHours() && call.time.getMinutes() === new Date().getMinutes())) {
-			return call
-		}
-
-		let arr = {
-			callback,
+		this.alarmCollection.push({
 			time,
-			canCall,
+			callback,
+			canCall: true,
+		})
+		if (this.canCall = true) {
+			this.callback = this.alarmCollection.filter(eliment => eliment.time === this.getCurrentFormattedTime())
 		}
-		this.alarmCollection.push(arr);
-
 	}
+
 	/*removeClock` — удаляет звонки по определённому времени. */
-	removeClock() {
-		if (this.alarmCollection.filter(call => call === time)) {
-			;
-			return call;
-		}
+
+	removeClock(time) {
+		return this.alarmCollection.filter(eliment => eliment.time !== time);
 	}
 
 	/*`getCurrentFormattedTime` — возвращает текущее время в строковом формате `HH:MM`. */
 	getCurrentFormattedTime() {
-		this.hours = new Date().getHours();
-		if (this.hours < 10) {
-			this.hours = '0' + this.hours;
-		}
-		this.minutes = new Date().getMinutes()
-		if (this.minutes < 10) {
-			this.minutes = '0' + this.minutes;
-		}
-		return `${this.hours}:${this.minutes}`;
+		return new Date().toLocaleTimeString("ru-Ru", {
+			hour: "2-digit",
+			minute: "2-digit"
+		});
 	}
 
 	/*`start` — запускает будильник. */
 	start() {
-		if (this.intervalId === true) {
+		if (this.intervalId) {
+			return stop();
+		}
+		if (!this.alarmCollection) {
 			this.intervalId = setInterval(() => {
-				this.alarmCollection.forEach(call => {
-					if (call.time === getCurrentFormattedTime()) {
-						call.callback();
+				this.alarmCollection.forEach(eliment => {
+					if (eliment.time === this.getCurrentFormattedTime() && this.canCall === true) {
+						return callback();
 					}
 				})
 			}, 1000);
-		} else {
-			return this.intervalId === false;
 		}
 	}
 
-	/*`stop` — останавливает выполнение интервала будильника. */
+	// /*`stop` — останавливает выполнение интервала будильника. */
 	stop() {
-		clearInterval = () => {
-			if (this.intervalId) {
-				this.intervalId.filter(call => call.intervalId === null);
-				return call
-			}
-		}
-	}
-
-	/*`resetAllCalls` — сбрасывает возможность запуска всех звонков. */
-	resetAllCalls() {
-		this.alarmCollection.forEach(call => call.time === null)
-
-	}
-	/* `clearAlarms` — удаляет все звонки. */
-	clearAlarms() {
-		stop();
-		this.alarmCollection = [];
+		clearInterval(this.intervalId);
 		this.intervalId = null;
+	}
+
+	// /*`resetAllCalls` — сбрасывает возможность запуска всех звонков. */
+	resetAllCalls() {
+		this.alarmCollection.forEach(eliment => eliment.canCall = true);
+
+	}
+
+	// /* `clearAlarms` — удаляет все звонки. */
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
 	}
 }
